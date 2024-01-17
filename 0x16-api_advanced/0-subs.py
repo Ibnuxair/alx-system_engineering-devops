94ac33e0df15e145876b83200eb813c4e39226e6
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-17 11:53:15 +0000

    Task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,33 +6,12 @@ A function that queries the Reddit API and returns the num of subscribers.
 import requests
 
 
-def subreddit_exists(subreddit):
-    """Check if subreddit exist."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditApp/1.0"}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-
-        # Check if the status code is 200
-        return response.status_code == 200
-
-    except requests.exceptions.RequestException as e:
-        # An exception occurred, indicating an issue with the request
-        return False
-
-
 def number_of_subscribers(subreddit):
     """Get the number of subscribers for a given subreddit."""
-    # Check if the subreddit exists before making the actual API call
-    if not subreddit_exists(subreddit):
-        return 0
-
-    # Continue with the original function
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditApp/1.0"}
 
+    # Custom User-Agent to avoid API request issues
+    headers = {"User-Agent": "MyRedditApp/1.0"}
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
