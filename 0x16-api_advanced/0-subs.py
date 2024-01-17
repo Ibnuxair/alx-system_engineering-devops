8ec8460df89a06b96f2adcb6b71ed15990592869
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-17 11:43:33 +0000

    Task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,33 @@ A function that queries the Reddit API and returns the num of subscribers.
 import requests
 
 
+def subreddit_exists(subreddit):
+    """Check if subreddit exist."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyRedditApp/1.0"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        # Check if the status code is 200
+        return response.status_code == 200
+
+    except requests.exceptions.RequestException as e:
+        # An exception occurred, indicating an issue with the request
+        return False
+
+
 def number_of_subscribers(subreddit):
     """Get the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # Check if the subreddit exists before making the actual API call
+    if not subreddit_exists(subreddit):
+        return 0
 
-    # Custom User-Agent to avoid API request issues
+    # Continue with the original function
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyRedditApp/1.0"}
+
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -19,8 +40,8 @@ def number_of_subscribers(subreddit):
         if response.status_code == 200:
             # Parse JSON response and extract the number of subscribers
             subreddit_data = response.json().get("data")
-            subscribers_count = subreddit_data.get("subscribers")
-            return subscribers_count if subscribers_count is not None else 0
+            subscribers_count = subreddit_data.get("subscribers", 0)
+            return subscribers_count
 
         else:
             return 0
