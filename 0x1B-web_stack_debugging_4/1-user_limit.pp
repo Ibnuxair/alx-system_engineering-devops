02550bf0d0a41d4f9750f930e41d61d4d7c89c36
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-28 02:56:27 +0000

    Task0

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,7 +5,7 @@ exec { 'change-os-configuration-for-holberton-user':
                   'NEW=hard nofile 50000',
 		  'OLD1=soft nofile 4',
 		  'NEW1=soft nofile 40000'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD1/$NEW2/" $DIR',
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD1/$NEW1/" $DIR',
   path        => ['/usr/bin', '/bin'],
   returns     => [0, 1]
 }
